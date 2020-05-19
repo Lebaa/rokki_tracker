b382271d16171a059be2ab7d1ff2d446f5a673ff
Mika Lehtinen <mika.lehtinen92@gmail.com>
2020-05-19 14:22:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
+import time
 from time import sleep
 import datetime
+import MySQLdb as mysql
+import sshtunnel
+
+sshtunnel.SSH_TIMEOUT = 5.0
+sshtunnel.TUNNEL_TIMEOUT = 5.0
+
 
 played = []
 
@@ -15,9 +22,36 @@ def Parser(nowPlaying):
     d = s[1].split("-",1)
     artist = d[0]
     song = d[1].lstrip()
-    time = str(datetime.datetime.now().time()).split(".")[0]
-    e = Entry(artist,song,time)
+    ts = time.time()
+    timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    e = Entry(artist,song,timestamp)
     return e
+
+def db_command(command, variables):
+    with sshtunnel.SSHTunnelForwarder(
+        ("ssh.eu.pythonanywhere.com"),
+        ssh_username="Leba",
+        ssh_password="Lebalol123",
+        remote_bind_address=("Leba.mysql.eu.pythonanywhere-services.com", 3306),
+    ) as tunnel:
+        connection = mysql.connect(
+            user="Leba",
+            password="superpassu",
+            host="127.0.0.1",
+            port=tunnel.local_bind_port,
+            database="Leba$rokki",
+        )
+
+        cur = connection.cursor()
+        cur.execute(command, variables)
+        result = cur.fetchall()
+        connection.commit()
+        connection.close()
+        return result
+
+def insert_biisi(artisti, kappale, timestamp):
+    sql = "INSERT INTO biisi (artisti, kappale, timestamp) VALUES (%s,%s,%s)"
+    db_command(sql, (artisti, kappale, timestamp))
 
 
 driver = webdriver.Firefox()
@@ -31,14 +65,17 @@ while datetime.datetime.now().hour > 5 and datetime.datetime.now().hour < 18:
 
     biisi = Parser(nowPlaying).song
     if biisi not in played:
-        if len(played) < 15:
-            played.append(Parser(nowPlaying).song)
+        if len(played) < 5:
+            obj = Parser(nowPlaying)
+            insert_biisi(obj.artist, obj.song, obj.timestamp)
+            played.append(obj.song)
             print(played)
             sleep(120)
         else:
             print("Vitusti kamaa listalla, tyhjennetään")
-            played.clear()
-            played.append(Parser(nowPlaying).song)
+            obj = Parser(nowPlaying)
+            insert_biisi(obj.artist, obj.song, obj.timestamp)
+            played.append(obj.song)
             print(played)
 
     else:
